9a376751c8c39e85edffa69e8fc8e2c8f13b7f9b
Letimineur <seb.colin91@gmail.com>
2022-10-24 21:34:29 +0200

    add default controller and properties for h2 and swagger

--- src/main/java/com/kata/rpncalculator/domain/RpnController.java
+++ src/main/java/com/kata/rpncalculator/domain/RpnController.java
@@ -1,2 +1,50 @@
-package com.kata.rpncalculator.domain;public class RpnController {
+package com.kata.rpncalculator.domain;
+
+import com.kata.rpncalculator.data.entity.CalculatorStack;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rpn")
+public class RpnController {
+
+
+    @GetMapping("/op")
+    public ResponseEntity<String> getAllOperators() {
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping("/stacks")
+    public ResponseEntity<List<CalculatorStack>> getAllStack() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping("/stack/{id}")
+    public ResponseEntity<CalculatorStack> getStackById(@PathVariable(name = "id") final Long id) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("/stack")
+    public ResponseEntity<CalculatorStack> createStack() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("/stack/{id}")
+    public ResponseEntity<CalculatorStack> pushInStack(@PathVariable(name = "id") final Long id, @RequestBody final double value) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("/op/{op}/stack/{id}")
+    public ResponseEntity<CalculatorStack> applyOperator(@PathVariable(name = "id") final Long id, @PathVariable(name = "op") final String op) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/stack/{id}")
+    public ResponseEntity<Void> deleteStack(@PathVariable(name = "id") final Long id) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
 }
