1b3d1aae04822308e0e79220e8873554b9bbf0be
Letimineur <seb.colin91@gmail.com>
2022-10-24 21:05:04 +0200

    spring initilizr

---
+++ src/test/java/com/kata/rpncalculator/RpncalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.rpncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpncalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
